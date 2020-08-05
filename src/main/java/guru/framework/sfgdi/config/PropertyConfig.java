2f395b055f644e3bd393c8c2f0884956d3832a7d
Yoav Susan <yoav.susan@personetics.com>
2020-08-05 08:35:38 +0300

    add properties configuration

---
+++ src/main/java/guru/framework/sfgdi/config/PropertyConfig.java
@@ -0,0 +1,46 @@
+package guru.framework.sfgdi.config;
+
+import guru.framework.sfgdi.examplebeans.FakeDataSource;
+import guru.framework.sfgdi.examplebeans.FakeJmsBroker;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PropertyConfig {
+
+    @Value("${guru.username}")
+    String username;
+    @Value("${guru.password}")
+    String password;
+    @Value("${guru.dburl}")
+    String url;
+
+    @Value("${guru.jms.username}")
+    String jmsUsername;
+    @Value("${guru.jms.password}")
+    String jmsPassword;
+    @Value("${guru.jms.dburl}")
+    String jmsUrl;
+
+    @Bean
+    public FakeDataSource fakeDataSource(){
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+
+        return fakeDataSource;
+    }
+
+    @Bean
+    public FakeJmsBroker fakeJmsBroker(){
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setUsername(jmsUsername);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUrl(jmsUrl);
+
+        return fakeJmsBroker;
+    }
+
+}
